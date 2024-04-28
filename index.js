c9f3a64eaad22f0f33374936b6b151d02ec7a4d1
Mohib <mohibullahmohim2020@gmail.com>
2024-04-28 13:20:03 +0600

    data send successFull in ui to server to database cool

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const touristSpot = [
-    {name: 'mohib'},
-    {name: 'mohihhhhh'},
-    {name: 'mohihhh'}
-]
+// const touristSpot = [
+//     {name: 'mohib'},
+//     {name: 'mohihhhhh'},
+//     {name: 'mohihhh'}
+// ]
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://mohib-the-maziest:IIKtxR9oAUzxxtTN@cluster0.ylmjbhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -60,9 +60,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('TRavelling server has been coming soon!')
+  res.send('Travelling server has been coming soon!')
 })
 
 app.listen(port, () => {
-  console.log(`My example app listening on port ${port}`)
+  console.log(`Travelling server is listening on port ${port}`)
 })
