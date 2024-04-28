987619d43f592716d73ba3ba3e650698787786b5
Mohib <mohibullahmohim2020@gmail.com>
2024-04-28 12:26:29 +0600

    server part is ready and added route

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+require ('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+const touristSpot = [
+    {name: 'mohib'},
+    {name: 'mohihhhhh'},
+    {name: 'mohihhh'}
+]
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mohib-the-maziest:IIKtxR9oAUzxxtTN@cluster0.ylmjbhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("spotstDB");
+    const allSpotsCollection = database.collection("allSpots");
+
+    app.get('/touristSpot', async(req, res) => {
+        const cursor = allSpotsCollection.find( );
+        const result = await cursor.toArray();
+        res.send(result)
+      })
+
+    app.post('/touristSpot', async(req, res) => {
+        const allSpot = req.body ;
+        console.log(allSpot);
+        const result = await allSpotsCollection.insertOne(allSpot);
+        res.send(result)
+    } )
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('TRavelling server has been coming soon!')
+})
+
+app.listen(port, () => {
+  console.log(`My example app listening on port ${port}`)
+})
