44b55ad04354d4e97b5dcc4ba1a8654ef3325997
Mohib <mohibullahmohim2020@gmail.com>
2024-04-30 02:26:17 +0600

    update get call

--- index.js
+++ index.js
@@ -47,12 +47,13 @@ async function run() {
       res.send(result)
     })
 
-    //   app.get('/touristSpot/:email', async(req, res) => {
-    //   const email = req.params.email ;
-    //   console.log(email)
-    //   const result = await coffeeCollection.findOne(email).toArray();
-    //   res.send(result)
-    // })
+    // update data 
+    app.get('/touristSpot/:id', async(req, res) => {
+      const id = req.params.id ;
+      const query = {_id : new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
+      res.send(result)
+    })
 
     app.post('/touristSpot', async(req, res) => {
         const allSpot = req.body ;
